175181208c77769b39580862c346da1a7494b127
cjbeowulf <baroonie_10@hotmail.com>
2015-09-13 03:33:24 -0700

    added create, show and edit

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,5 +1,11 @@
 class ComicsController < ApplicationController
+	before_action :find_comic, only: [:show, :edit, :update, :destroy]
 	def index
+		@comics = Comic.all.order('created_at DESC')
+	end
+
+	def show
+		
 	end
 
 	def new
@@ -9,10 +15,35 @@ class ComicsController < ApplicationController
 	def create
 		@comic = Comic.new(comic_params)
 		
+		if @comic.save
+			redirect_to @comic, notice: 'Successfully created new comic strip'
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @comic.update(comic_params)
+			redirect_to @comic, notice: 'Comic Successfully updated'
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		
 	end
 
 	private
 	def comic_params
-		params.require(:comic.permit[:title, :description])
+		params.require(:comic).permit(:title, :description)
+	end
+
+	def find_comic
+		@comic = Comic.find(params[:id])
 	end
 end
